d54fcdfeeffc5be10d452bee1ab7a208bbbc0e5f
Patrick510 <patrickdedinho321xz@gmail.com>
2025-01-16 09:50:43 -0400

    Excercise finally done

--- script.js
+++ script.js
@@ -7,64 +7,61 @@
 6 - Operações de Agregação - Realizar operações de agregação como soma, contagem, e agrupamento.
 */
 
+// Importando dados
+mongoimport --db bdTrabalho --collection myCollection --file dados.json --jsonArray
+
 // Conecta o mongoDB
 use bdTrabalho;
 
-// Importando dados
-mongoimport --db bdTrabalho --collection minhaColecao --file dados.json --jsonArray
-
-// Inserir os dados importados na coleção
-db.suaColecao.insertMany(dadosImportados);
-
 // Inserir um novo documento
-db.suaColecao.insertOne({
+db.myCollection.insertOne({
   nome: "Carlos Mendes",
   idade: 28,
   cidade: "Belo Horizonte"
 });
 
 // Inserir múltiplos documentos
-db.suaColecao.insertMany([
+db.myCollection.insertMany([
   { nome: "Ana Paula", idade: 35, cidade: "Fortaleza" },
   { nome: "Paulo André", idade: 22, cidade: "Curitiba" }
 ]);
 
 // Atualizar um único documento
-db.suaColecao.updateOne(
-  { nome: "Carlos Mendes" }, // Filtro
-  { $set: { idade: 29 } } // Atualização
+db.myCollection.updateOne(
+  { nome: "Carlos Mendes" },
+  { $set: { idade: 29 } }
 );
 
 // Atualizar múltiplos documentos
-db.suaColecao.updateMany(
-  { cidade: "São Paulo" }, // Filtro
-  { $set: { cidade: "Salvador" } } // Atualização
+db.myCollection.updateMany(
+  { cidade: "São Paulo" },
+  { $set: { cidade: "Salvador" } }
 );
 
 // Excluir um único documento
-db.suaColecao.deleteOne({ nome: "Paulo" });
+db.myCollection.deleteOne({ nome: "Paulo" });
 
 // Excluir múltiplos documentos
-db.suaColecao.deleteMany({ idade: { $lt: 30 } });
+db.myCollection.deleteMany({ idade: { $lt: 30 } });
 
 // Consultar todos os documentos
-db.suaColecao.find();
+db.myCollection.find();
 
 // Consultar documentos com filtro (ex: todos com idade > 30)
-db.suaColecao.find({ idade: { $gt: 30 } });
+db.myCollection.find({ idade: { $gt: 30 } });
 
 // Consultar e projetar apenas campos específicos
-db.suaColecao.find({ cidade: "São Paulo" }, { nome: 1, idade: 1 });
+db.myCollection.find({ cidade: "São Paulo" }, { nome: 1, idade: 1 });
 
 // Contar o número de pessoas por cidade
-db.suaColecao.aggregate([
+db.myCollection.aggregate([
   { $group: { _id: "$cidade", total: { $sum: 1 } } }
 ]);
 
-// Média de idade por cidade
-db.suaColecao.aggregate([
+// Média de idade por cidade - Ordena por média de idade (decrescente)
+db.myCollection.aggregate([
   { $group: { _id: "$cidade", mediaIdade: { $avg: "$idade" } } },
-  { $sort: { mediaIdade: -1 } }  // Ordenar por média de idade (decrescente)
+  { $sort: { mediaIdade: -1 } } 
 ]);
 
 // Filtrar cidades com media maior que 36
